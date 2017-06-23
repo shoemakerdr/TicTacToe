345e70eae60f4f76c35c6dac4d9a0e267e75b5e5
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-23 20:58:55 +0000

    Added inline docs. Replaced setBoard method with optional param in constructor.

--- src/tictactoe/board.js
+++ src/tictactoe/board.js
@@ -1,22 +1,35 @@
 'use strict';
 
-function Board() {
-    this.spaces = [null,null,null,null,null,null,null,null,null];
+/**
+ * Gives a tic tac toe board object
+ * 
+ * @constructor
+ * @param {Array} spaces Optional array of spaces used for testing.
+ */
+function Board(spaces) {
+    this.spaces = spaces || [null,null,null,null,null,null,null,null,null];
 }
 
+/**
+ * Gives a tic tac toe board object
+ * 
+ * @constructor
+ * @param {Array} spaces Optional array of spaces used for testing.
+ */
 Board.prototype.addMove = function(letter, position) {
     this.spaces[position - 1] = letter;
 };
 
+/**
+ * Checks whether every space on board is occupied
+ * 
+ * @return {Boolean}
+ */
 Board.prototype.isFull = function() {
     for (let space of this.spaces) {
         if (space === null) return false;
     }
     return true;
 };
-
-Board.prototype.setBoard = function(array) {
-    this.spaces = array;
-}
 
 module.exports = Board;
