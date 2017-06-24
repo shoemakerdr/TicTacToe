9ddff289e7f963d68374a04fca0ca7fe229f3a63
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-23 23:47:41 -0500

    Added name parameter to Player constrctor.

--- src/tictactoe/player.js
+++ src/tictactoe/player.js
@@ -5,28 +5,30 @@ const prompt = require("prompt-sync")({autocomplete: false});
 /**
  * Takes a player type which provides determines how the player object will
  * choose moves
- * 
+ *
  * @constructor
  * @param {String} type Either 'human' or 'ai'
+ * @param {String} name Name of player or will default to type if not specified
  */
-function Player(type) {
+function Player(type, name) {
     this.type = type;
+    this.name = name || type;
     this.chooseMove = type === 'human' ? human : ai;
-    
+
     function randomIndex(len) {
         return Math.floor(Math.random() * len);
     }
-    
+
     function availableMoves(spaces) {
             return spaces.map((item, i) => (item === null) ? i : item)
                          .filter(item => typeof item === 'number');
     }
-    
+
     function ai({spaces}) {
         const avail = availableMoves(spaces);
         return avail[randomIndex(avail.length)];
     }
-    
+
     function human({spaces}) {
         const avail = availableMoves(spaces);
         console.log('Here are the available moves: ');
