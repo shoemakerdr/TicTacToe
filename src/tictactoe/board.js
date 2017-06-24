54e61bb6a53efe7d3b9154c9e797710578761ff8
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-23 21:41:50 -0500

    Added isOccupied method for individual spaces.

--- src/tictactoe/board.js
+++ src/tictactoe/board.js
@@ -2,7 +2,7 @@
 
 /**
  * Gives a tic tac toe board object
- * 
+ *
  * @constructor
  * @param {Array} spaces Optional array of spaces used for testing.
  */
@@ -12,7 +12,7 @@ function Board(spaces) {
 
 /**
  * Gives a tic tac toe board object
- * 
+ *
  * @constructor
  * @param {Array} spaces Optional array of spaces used for testing.
  */
@@ -21,13 +21,26 @@ Board.prototype.addMove = function(letter, position) {
 };
 
 /**
+ * Checks whether given space or index on board is occupied
+ *
+ * @param {Any} space Either an index of the board array or item from board
+ * array
+ * @return {Boolean}
+ */
+ Board.prototype.isOccupied = function (space) {
+    return (space === Number(space))
+            ? this.spaces[space] !== null
+            : space !== null;
+ };
+
+/**
  * Checks whether every space on board is occupied
- * 
+ *
  * @return {Boolean}
  */
 Board.prototype.isFull = function() {
     for (let space of this.spaces) {
-        if (space === null) return false;
+        if (!this.isOccupied(space)) return false;
     }
     return true;
 };
