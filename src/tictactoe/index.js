4a2aa08d5863355c70de19f5fd4f162ab8f1f6c4
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-23 23:45:56 -0500

    Added twoplayer and oneplayer functions that start game.

--- src/tictactoe/index.js
+++ src/tictactoe/index.js
@@ -2,34 +2,30 @@
 
 const util = require('util');
 const Board = require("./board");
-// const player = require("./player");
+const Player = require("./player");
 const Game = require("./game");
-const prompt = require("prompt-sync")();
-
-const cl = console.log;
-
-const b = new Board();
-const g = new Game(b,'x', 'o');
-
-// play();
-
-function checkGame() {
-    cl(g.showBoard());
-    if (g.isGameOver()) {
-        cl(g.results());
-        process.exit();
-    }
-}
+const prompt = require("prompt-sync")({autocomplete: false});
 
 /**
  * TODO: write one player version
  */
 function oneplayer() {
-    return;
+    const name = prompt('What is your name? ');
+    const b = new Board();
+    const p1 = new Player('human', name);
+    const p2 = new Player('ai');
+    const g = new Game(b, p1, p2);
+    g.start();
 }
 
 function twoplayer() {
-    play();
+    const name1 = prompt('What is the name of the first player? ');
+    const name2 = prompt('What is the name of the second player? ');
+    const b = new Board();
+    const p1 = new Player('human', name1);
+    const p2 = new Player('human', name2);
+    const g = new Game(b, p1, p2);
+    g.start();
 }
 
 function takeTurn(letter) {
@@ -40,8 +36,10 @@ function takeTurn(letter) {
 }
 
 function play() {
-    cl(g.showBoard());
-    takeTurn('x');
+    const numPlayers = prompt('How many players? (1 or 2) ');
+    (numPlayers === '2')
+        ? twoplayer()
+        : oneplayer();
 }
 
-module.exports = {oneplayer, twoplayer};
+module.exports.play = play;
