0c8d26879bf2e617eb84c40be8e09a8f92b45e68
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-22 21:03:45 +0000

    removed rowStyle and replaced with div wrapper around row

--- src/Board.js
+++ src/Board.js
@@ -2,18 +2,13 @@ import React from 'react'
 import './Board.css'
 import Row from './Row'
 
-const styles = {
-    centerRow: {
-        borderTop: '2px solid #888',
-        borderBottom: '2px solid #888'
-    }
-}
-
 const Board = props => (
         <div className='Board-wrapper'>
             <div className='Board'>
                 <Row />
-                <Row rowStyle={styles.centerRow}/>
+                <div className='row-center'>
+                    <Row />
+                </div>
                 <Row />
             </div>
         </div>
