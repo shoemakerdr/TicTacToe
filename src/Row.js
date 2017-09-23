3aeddb301297115945cf7fad77d515ec938bd72e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-23 14:44:43 -0500

    Imported XImage and OImage components as board pieces.

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Row.css'
+import XImage from './XImage'
+import OImage from './OImage'
 
 const Row = props => (
         <div className='Row' style={props.rowStyle}>
-            <div id='space00' className='space space-left'>X</div>
-            <div id='space01' className='space space-center'>O</div>
-            <div id='space02' className='space space-right'>X</div>
+            <div id='space00' className='space space-left'><XImage /></div>
+            <div id='space01' className='space space-center'><OImage /></div>
+            <div id='space02' className='space space-right'><XImage /></div>
         </div>
     )
 
