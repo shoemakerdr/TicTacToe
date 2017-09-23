3f8a5637dd0af72d00d984dcf1cc99d13a36ab40
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-23 14:39:08 -0500

    Added X and O piece components

---
+++ src/OImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './OImage.css'
+import oPNG from './o.png'
+
+const OImage = props => (
+        <img className='OImage' src={oPNG} alt='O' />
+    )
+
+export default OImage
