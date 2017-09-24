9fe7fec1499b2d109a000712987e7f04651060c6
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-24 01:04:52 -0500

    Added props and click handlers for each space.

--- src/Row.js
+++ src/Row.js
@@ -5,9 +5,24 @@ import OImage from './OImage'
 
 const Row = props => (
         <div className='Row' style={props.rowStyle}>
-            <div id='space00' className='space space-left'><XImage /></div>
-            <div id='space01' className='space space-center'><OImage /></div>
-            <div id='space02' className='space space-right'><XImage /></div>
+            <div
+                onClick={props.leftHandler}
+                className='space space-left'
+            >
+                {props.left === 'x' ? <XImage /> : props.left === 'o' ? <OImage /> : '' }
+            </div>
+            <div
+                onClick={props.centerHandler}
+                className='space space-center'
+            >
+                {props.center === 'x' ? <XImage /> : props.center === 'o' ? <OImage /> : '' }
+            </div>
+            <div
+                onClick={props.rightHandler}
+                className='space space-right'
+            >
+                {props.right === 'x' ? <XImage /> : props.right === 'o' ? <OImage /> : '' }
+            </div>
         </div>
     )
 
