e0d2342ef05318ed8dd4e8912233e3a702cbef5a
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-24 15:04:26 -0500

    Replaced XImage and OImage components with Player component

--- src/Row.js
+++ src/Row.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import './Row.css'
-import XImage from './XImage'
-import OImage from './OImage'
+import Player from './Player'
 
 const Row = props => (
-        <div className='Row' style={props.rowStyle}>
-            <div
-                onClick={props.leftHandler}
-                className='space space-left'
-            >
-                {props.left === 'x' ? <XImage /> : props.left === 'o' ? <OImage /> : '' }
-            </div>
-            <div
-                onClick={props.centerHandler}
-                className='space space-center'
-            >
-                {props.center === 'x' ? <XImage /> : props.center === 'o' ? <OImage /> : '' }
-            </div>
-            <div
-                onClick={props.rightHandler}
-                className='space space-right'
-            >
-                {props.right === 'x' ? <XImage /> : props.right === 'o' ? <OImage /> : '' }
-            </div>
+    <div className='Row' style={props.rowStyle}>
+        <div
+            onClick={props.leftHandler}
+            className='space space-left'
+        >
+            <Player player={props.left} />
         </div>
-    )
+        <div
+            onClick={props.centerHandler}
+            className='space space-center'
+        >
+            <Player player={props.center} />
+        </div>
+        <div
+            onClick={props.rightHandler}
+            className='space space-right'
+        >
+            <Player player={props.right} />
+        </div>
+    </div>
+)
 
 export default Row
