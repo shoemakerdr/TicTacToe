ff14b94f1caebea611e2fe615c421aa084546e9d
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-24 15:06:41 -0500

    Generalized Modal component structure

--- src/Modal.js
+++ src/Modal.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import './Modal.css'
+import Animation from './Animation'
 
-const gameSetup = {
-    message: 'What kind of game do you want to play?',
-    buttons: ['Two Player', 'Easy', 'Unbeatable']
+const styles = {
+    visible: {
+        display: 'flex'
+    },
+    hidden: {
+        display: 'none'
+    }
 }
 
 const Modal = props => (
-        <div className='Modal'>
-            <div className='message'>
-                <div className='prompt'>{gameSetup.message}</div>
-                <div className='buttons'>
-                    {gameSetup.buttons.map((button,index) => (
+    <div
+        className='Modal'
+        style={props.struct.message ? styles.visible : styles.hidden}
+    >
+        <div className='message'>
+            <div className='prompt'>{props.struct.message}</div>
+            <div className='buttons'>
+                {props.struct.animation
+                    ? <Animation />
+                    : props.struct.buttons.map((button,index) => (
                         <div
                             key={index}
+                            onClick={button.action}
                             className='button'
                         >
-                            {button}
+                            {button.name}
                         </div>
-                    ))}
-                </div>
+                ))}
             </div>
         </div>
-        
-    )
+    </div>
+)
 
 export default Modal
